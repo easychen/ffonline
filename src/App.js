2eb953f36e4906a3663265d03db9afcd3eb7d388
EasyChen <easychen@qq.com>
2019-09-29 20:38:44 +0800

    init

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Login from './screen/Login';
+import Test from './screen/Test'; 
+import Test2 from './screen/Test2'; 
+
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route path="/" component={Login} />
+          <Route path="/t2" component={Test2} />
+          <Route path="/" component={Test} />
+          <Route path="/Login" component={Login} />
         </Switch>
       </Router>  
     );
